051aa9aa55fea88dd7c731301a3ef1d566460e95
Will Crawford <378681+sapid@users.noreply.github.com>
2017-12-04 21:54:34 -0800

    Added many debugging statements to trace execution

--- README.md
+++ README.md
@@ -2,13 +2,15 @@
 
 This is a WIP, being adapted from the forked source (a Tornado adapter) to work with Twisted. It DOES NOT WORK right now.
 
-Current state: `src/protocol.py` initiates a connection
+Current state: `src/protocol.py` initiates a connection and authenticates.
+
+THIS BRANCH MAY BE FORCE-PUSHED AT ANY TIME.
+
+I intend to clean up the commit history and the structure of this repository.
 
 Outstanding priority issues:
-* message_parser expects a client object to send errors to.
-* heartbeat isn't handled yet; requires message_builder to be fixed first.
-* auth needs to get sent.
 * handler router initialization needs to be finished in factory init.
+* server still dropping connection :\ 
 
 # deepstreampy
 A Python client for deepstream.io for Twisted.
--- deepstreampy_twisted/protocol.py
+++ deepstreampy_twisted/protocol.py
@@ -1,40 +1,69 @@
 #!/usr/bin/env python
 from autobahn.twisted.websocket import WebSocketClientProtocol, WebSocketClientFactory
+from twisted.internet import task as twisted_task
 from twisted.internet.protocol import ReconnectingClientFactory
 from deepstreampy_twisted import constants
+import time
 from deepstreampy_twisted.message import message_parser, message_builder
+import txaio
+import inspect # TODO: For debug only
+
+class ErrorCatcher(object):
+    def __init__(self, func):
+        self._on_error = func
 
 class DeepstreamProtocol(WebSocketClientProtocol):
     # Protocols are used once per session; they are not re-used. 
     # If reconnection occurs, a new protocol is created by the factory in use.
     def onConnect(self, response):
-        # TODO: Anything to do?
+        print(inspect.stack()[0][3])
         # Options:
         #  Check or set cookies or other HTTP headers
         #  Verify IP address
         # TODO: Verify we're speaking Deepstream, a subprotocol of WebSocket, essentially
+        #       Update: may not be possible; there doesn't seem to be an "info" call or anything
         print("Connected to Server: {}".format(response.peer))
+        # TODO: Register handlers
         self.factory._set_state(constants.connection_state.AWAITING_CONNECTION)
         self.debug("onConnect response: {}".format(response))
     def onOpen(self):
-        # TODO: If we have auth information stored, negotiate authentication again.
+        print(inspect.stack()[0][3])
         # TODO: If auth is open, we don't need auth information to login. 
         #         How can we check if auth is open? Attempt to login anyway and catch exceptions?
         #         Per https://deepstream.io/info/protocol/all-messages/ it may not be possible to query for state.
-        # TODO: Switch flag to start flushing message queue as well?
-            self.debug("Connection opened.")
-            self.factory._set_state(constants.connection_state.AUTHENTICATING)
-            self.authenticate()
-        # TODO: Initialize heartbeat
-        # For looping calls see http://twistedmatrix.com/documents/13.2.0/core/howto/time.html
-        # l = twisted.internet.loopingCall(func_to_call)
-        # l.start(self.factory._heartbeat_time) # TODO default should be?
-        # self.factory._heartbeat_looper = l
+        self.debug("Connection opened.")
+        # TODO: Register handlers
+        self._heartbeat_last = time.time()
+        l = twisted_task.LoopingCall(self._heartbeat)
+        self.factory._heartbeat_looper = l
         # TODO: Reset retry timer in reconnectfactory.
+    def _catch_error(self, topic, event, msg=None):
+        print(inspect.stack()[0][3])
+        # Adapted from https://github.com/YavorPaunov/deepstreampy under MIT License
+        if event in (constants.event.ACK_TIMEOUT,
+                     constants.event.RESPONSE_TIMEOUT):
+            if (self.factory._state == constants.connection_state.AWAITING_AUTHENTICATION):
+                error_msg = ("Message timed out because authentication is incomplete.")
+                self.factory.reactor.callLater(0.1, lambda: self._catch_error(event.NOT_AUTHENTICATED,
+                                                                              constants.topic.ERROR,
+                                                                              error_msg))
+        # TODO: If we end up having listeners for errors, notify them here.
+        raw_error_message = event + ': ' + msg
+        if topic:
+            raw_error_message += ' (' + topic + ')'
+        raise ValueError(raw_error_message)
+    def send(self, message):
+        print(inspect.stack()[0][3])
+        # TODO: Buffer messages
+        if isinstance(message, unicode):
+            message = message.encode("utf-8")
+        self.sendData(message)
+
     def onMessage(self, payload, isBinary):
+        print(inspect.stack()[0][3])
         if isBinary:
             raise NotImplementedError
-        self.debug(payload)
+        self.debug("Received: " + payload)
         # TODO: Do we need to catch decoding errors?
         #text = payload.decode('UTF-8', errors='strict') # Unnecessary?
         full_buffer = self.factory._message_buffer + payload
@@ -43,33 +72,141 @@ class DeepstreamProtocol(WebSocketClientProtocol):
             self.factory._message_buffer = split_buffer[1]
         raw_messages = split_buffer[0]
         # TODO Urgent: Uncomment below once message_parser is fixed
-        #parsed_messages = message_parser.parse(raw_messages, self._client)
-        #for msg in parsed_messages:
-        #    if msg is None:
-        #        continue
+        parsed_messages = message_parser.parse(raw_messages, ErrorCatcher(self._catch_error))
+        for msg in parsed_messages:
+            if msg is None:
+                continue
+            elif msg['topic'] == constants.topic.CONNECTION:
+                self._connection_response_handler(msg)
+            elif msg['topic'] == constants.topic.AUTH:
+                self._auth_response_handler(msg)
+            else:
+                raise NotImplementedError(msg) # TODO: Send to client object?
 
-    def heartbeat(self):
-        raise NotImplementedError
+
+    def _heartbeat(self):
+        print(inspect.stack()[0][3])
+        elapsed = time.time() - self.factory._heartbeat_last
+        if elapsed >= self.factory._heartbeat_tolerance:
+            self.debug("Heartbeat missed. Closing connection.")
+            self.transport.loseConnection()
+            # TODO: Inform logger
         # TODO: Check if we've missed any.
     def authenticate(self):
-        # TODO: Write this
-        self.factory._set_state(constants.connection_state.AWAITING_AUTHENTICATION)
-    def _auth_response_handler(self, data):
-        pass
+        # Dead code: we automatically authenticate when we receive "C|A+"
+        #            instead of making our client call authentication manually
+        print(inspect.stack()[0][3])
+        self._auth_params = self.factory.authParams
+        self._auth_future = concurrent.Future() # Change to deferred? Do we need this?
+
+        # if (self._too_many_auth_attempts or
+        #         self._challenge_denied or
+        #         self._connection_auth_timeout):
+        #     msg = "this client's connection was closed"
+        #     self._client._on_error(constants.topic.ERROR,
+        #                            constants.event.IS_CLOSED,
+        #                            msg)
+        #     self._auth_future.set_result(
+        #         {'success': False,
+        #          'error': constants.event.IS_CLOSED,
+        #          'message': msg})
+        #
+        # elif (self._deliberate_close and
+        #               self._state == constants.connection_state.CLOSED):
+        #     self.connect()
+        #     self._deliberate_close = False
+        #     self._client.once(constants.event.CONNECTION_STATE_CHANGED,
+        #                       lambda: self.authenticate(auth_params))
+
+        if self.factory._state == constants.connection_state.AWAITING_AUTHENTICATION:
+            self._send_auth_params()
+
+        return self._auth_future
+        self.factory._set_state(constants.connection_state.AUTHENTICATING)
+    def _send_auth_params(self):
+        print(inspect.stack()[0][3])
+        self.factory._set_state(constants.connection_state.AUTHENTICATING)
+        raw_auth_message = message_builder.get_message(
+            constants.topic.AUTH,
+            constants.actions.REQUEST,
+            [self.factory.authParams]
+        )
+        self.send(raw_auth_message)
+    def _get_auth_data(self, data):
+        print(inspect.stack()[0][3])
+        if data:
+            return message_parser.convert_typed(data, ErrorCatcher(self._catch_error()))
+    def _auth_response_handler(self, message):
+        message_data = message['data']
+        message_action = message['action']
+        data_size = len(message_data)
+        if message_action == constants.actions.ERROR:
+            if (message_data and
+                message_data[0] == constants.event.TOO_MANY_AUTH_ATTEMPTS):
+                self.transport.loseConnection() # TODO: Prevent reconnection
+            else:
+                self.factory._set_state(
+                    constants.connection_state.AWAITING_AUTHENTICATION)
+            auth_data = (self._get_auth_data(message_data[1]) if
+                         data_size > 1 else None)
+            if self._auth_future:
+                self._auth_future.set_result(
+                    {'success': False,
+                     'error': message_data[0] if data_size else None,
+                     'message': auth_data}
+                )
+        elif message_action == constants.actions.ACK:
+            self.factory._set_state(constants.connection_state.OPEN)
+            auth_data = (self._get_auth_data(message_data[0]) if
+                        data_size else None)
+            if self._auth_future:
+                self._auth_future.set_result(
+                    {'success': True,
+                     'error': None,
+                     'message': auth_data}
+                )
+
+        # TODO: Flush outgoing message queue
+    def _connection_response_handler(self, message):
+        print(inspect.stack()[0][3])
+        action = message['action']
+        data = message['data']
+        if action == constants.actions.PING:
+            self._heartbeat_last = time.time()
+            ping_response = message_builder.get_message(
+                constants.topic.CONNECTION, constants.actions.PONG)
+            self.send(ping_response)
+        elif action == constants.actions.ACK:
+            self.factory._set_state(constants.connection_state.AWAITING_AUTHENTICATION)
+            self._send_auth_params()
+        elif action == constants.actions.CHALLENGE:
+            challenge_response = message_builder.get_message(
+                constants.topic.CONNECTION,
+                constants.actions.CHALLENGE_RESPONSE,
+                [self.factory.url])
+            self.factory._set_state(constants.connection_state.CHALLENGING)
+            self.send(challenge_response)
     def onClose(self, wasClean, code, reason):
+        print(inspect.stack()[0][3] + " from " + inspect.stack()[1][3])
         # TODO: Anything to do?
+        #       Should state be set to ERROR if wasClean is false?
         self.factory._set_state(constants.connection_state.CLOSED)
         print("WebSocket connection closed: {}".format(reason))
-        # if self.factory._heartbeat_looper:
-        #   self.factory._heartbeat_looper.cancel()
-        #   self.factory._heartbeat_looper = None
+        if self.factory._heartbeat_looper and self.factory._heartbeat_looper.running:
+            self.factory._heartbeat_looper.stop()
+        self.factory._heartbeat_looper = None
+        self.factory._heartbeat_last = None
     def sendData(self, data):
+        print(inspect.stack()[0][3])
         # TODO: Batch messages
-        self.transport.write(data)
+        super(DeepstreamProtocol, self).sendData(data)
         self.debug("Sent " + str(data))
     def debug(self, message):
+        print(inspect.stack()[0][3])
         if not self.factory.debug:
             return
+        if isinstance(message, str):
+            message = unicode(message, 'utf-8')
         print(message.replace(chr(31), '|').replace(chr(30), '+'))
 
 class DeepstreamFactory(WebSocketClientFactory, ReconnectingClientFactory):
@@ -77,8 +214,9 @@ class DeepstreamFactory(WebSocketClientFactory, ReconnectingClientFactory):
     # This way, if reconnection occurs, that state information is still available to the new protocol.
     # In the Twisted paradigm, the factory is initialized then handed off to the reactor which initiates the connection.
     # TODO note: Important for message queue: self.(connection?).
-    handlers_needed = [constants.topic.CONNECTION,
-                       constants.topic.AUTH,
+    handlers_needed = [
+                       #constants.topic.CONNECTION,
+                       #constants.topic.AUTH,
                        constants.topic.EVENT,
                        constants.topic.PRIVATE,
                        constants.topic.RPC,
@@ -87,14 +225,25 @@ class DeepstreamFactory(WebSocketClientFactory, ReconnectingClientFactory):
                        constants.topic.RECORD,
                        ]
     def __init__(self, url, *args, **kwargs):
+        # url: (str) the URL to connect to
+        # debug: (bool) (optional) print debug messages
+        # heartbeat_interval: (double) (optional) interval to check heartbeat
+        # auth_params: (dict) # TODO: Document structure expected
+        # authCallback (func) # Callback triggered by successful authentication
         self.url = url
         self._state = constants.connection_state.CLOSED
         kwargs['url'] = url
         self.debug = kwargs.pop('debug', False)
+        self._heartbeat_interval = kwargs.pop('heartbeat_interval', 100)
+        self._heartbeat_tolerance = self._heartbeat_interval * 2
         self._heartbeat_looper = None
+        self._heartbeat_last = None
         self._message_buffer = ''
         self.authParams = kwargs.pop('authParams', None)
         self.authToken = None
+        #self.authCallback = kwargs.pop('authCallback', None)
+        self._auth_future = None # TODO: Convert to Deferred
+        # TODO: Create handler dict
         super(DeepstreamFactory, self).__init__(*args, **kwargs)
     def _set_state(self, state):
         # This state keeps track of the connection with Deepstream per the
@@ -121,7 +270,7 @@ if __name__ == '__main__':
     from autobahn.twisted.websocket import WebSocketClientFactory,WebSocketClientProtocol
 
     log.startLogging(sys.stdout)
-    factory = DeepstreamFactory(u"ws://localhost:6020/deepstream", debug=True)
+    factory = DeepstreamFactory("ws://localhost:6020/deepstream", debug=True)
     # TODO: Set auth information for factory
     factory.protocol = DeepstreamProtocol
     reactor.connectTCP("127.0.0.1", 6020, factory)
